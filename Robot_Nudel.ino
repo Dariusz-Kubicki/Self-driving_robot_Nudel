081fa1b3c1e608477bc961ff3dcf5a870ba89449
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-07-20 17:13:42 +0200

    Add files via upload

---
+++ Robot_Nudel.ino
@@ -0,0 +1,27 @@
+// Piny czujników IR
+#define IR_Lewy 8
+#define IR_Srodek_Lewy 7
+#define IR_Srodek_Prawy 4
+#define IR_Prawy 2
+
+// Piny silników
+#define AIN1 5
+#define AIN2 3
+#define BIN1 6
+#define BIN2 9
+
+void setup() {
+  pinMode(IR_Lewy, INPUT);
+  pinMode(IR_Srodek_Lewy, INPUT);
+  pinMode(IR_Srodek_Prawy, INPUT);
+  pinMode(IN_Prawy, INPUT);
+
+  pinMode()
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
